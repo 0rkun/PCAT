e77332d9e359b5587212c18c3d20a763fe8a8c7b
Orkun Yılmaz <106023667+0rkun@users.noreply.github.com>
2023-04-11 17:49:33 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function () {
-  console.log('home working');
+const express = require('express');
+
+const app = express();
+//middleware
+const myLogger = (req, res, next) => {
+  console.log('Middleware');
+  next();
 };
+
+//middleware
+app.use(express.static('public')); //bir public dosyası oluşturuyoruz
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo desc',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sayfa ${port} portunda çalışıyor.`);
+});
